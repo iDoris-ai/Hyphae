AuraAIHQ/agent-speaker

feat(profile): v0.25.0 agent profile publish/discover
- Add AgentProfile type with capabilities, availability, rate sheet
- Add profile publish command to nostr relays (Kind 30078 extension)
- Add profile discover, show, list, search commands
- Add SQLite-backed profile caching
- 100% unit test coverage for internal/profile and pkg/types
- Real E2E tests with alice/bob on relay.aastar.io
- Add .claude/AGENTS.md for agent design docs

cmd/agent-speaker/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/jason/agent-speaker/internal/identity"
 	"github.com/jason/agent-speaker/internal/messaging"
 	"github.com/jason/agent-speaker/internal/nostr"
+	"github.com/jason/agent-speaker/internal/profile"
 	"github.com/jason/agent-speaker/internal/storage"
 	"github.com/jason/agent-speaker/internal/tui"
 	"github.com/urfave/cli/v3"
@@ -47,6 +48,8 @@ func main() {
 			tui.TUICmd,
 			// Group Chat
 			group.GroupCmd,
+			// Agent Profile
+			profile.ProfileCmd,
 			// Daemon
 			daemon.DaemonCmd,
 		},

internal/profile/db.go
@@ -1,0 +1,208 @@
+package profile
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/jason/agent-speaker/internal/storage"
+	"github.com/jason/agent-speaker/pkg/types"
+)
+
+// DB wraps profile database operations
+type DB struct {
+	db *sql.DB
+}
+
+// NewDB creates a new profile DB instance
+func NewDB() (*DB, error) {
+	db, err := storage.InitDB()
+	if err != nil {
+		return nil, err
+	}
+
+	p := &DB{db: db}
+	if err := p.migrate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// NewDBWithPath creates a new profile DB instance for testing
+func NewDBWithPath(dbPath string) (*DB, error) {
+	db, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	if _, err := db.Exec("PRAGMA foreign_keys = ON"); err != nil {
+		return nil, fmt.Errorf("failed to enable foreign keys: %w", err)
+	}
+
+	p := &DB{db: db}
+	if err := p.migrate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// migrate creates profile tables
+func (p *DB) migrate() error {
+	_, err := p.db.Exec(`
+		CREATE TABLE IF NOT EXISTS agent_profiles (
+			npub TEXT PRIMARY KEY,
+			name TEXT NOT NULL,
+			description TEXT,
+			availability TEXT,
+			profile_json TEXT NOT NULL,
+			updated_at INTEGER NOT NULL,
+			discovered_at INTEGER NOT NULL
+		)
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to create agent_profiles table: %w", err)
+	}
+
+	indexes := []string{
+		`CREATE INDEX IF NOT EXISTS idx_profiles_name ON agent_profiles(name)`,
+		`CREATE INDEX IF NOT EXISTS idx_profiles_availability ON agent_profiles(availability)`,
+		`CREATE INDEX IF NOT EXISTS idx_profiles_updated ON agent_profiles(updated_at DESC)`,
+	}
+
+	for _, idx := range indexes {
+		if _, err := p.db.Exec(idx); err != nil {
+			return fmt.Errorf("failed to create index: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// StoreProfile stores or updates an agent profile
+func (p *DB) StoreProfile(npub string, profile *types.AgentProfile) error {
+	jsonData, err := json.Marshal(profile)
+	if err != nil {
+		return fmt.Errorf("failed to marshal profile: %w", err)
+	}
+
+	_, err = p.db.Exec(`
+		INSERT OR REPLACE INTO agent_profiles (npub, name, description, availability, profile_json, updated_at, discovered_at)
+		VALUES (?, ?, ?, ?, ?, ?, COALESCE((SELECT discovered_at FROM agent_profiles WHERE npub = ?), ?))
+	`, npub, profile.Name, profile.Description, profile.Availability, string(jsonData), profile.UpdatedAt, npub, profile.UpdatedAt)
+
+	if err != nil {
+		return fmt.Errorf("failed to store profile: %w", err)
+	}
+
+	return nil
+}
+
+// GetProfile retrieves a profile by npub
+func (p *DB) GetProfile(npub string) (*types.AgentProfile, error) {
+	var jsonData string
+	err := p.db.QueryRow(
+		"SELECT profile_json FROM agent_profiles WHERE npub = ?",
+		npub,
+	).Scan(&jsonData)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get profile: %w", err)
+	}
+
+	profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
+}
+
+// ListProfiles lists all stored profiles
+func (p *DB) ListProfiles() ([]*types.StoredProfile, error) {
+	rows, err := p.db.Query(`
+		SELECT npub, name, description, availability, profile_json, updated_at, discovered_at
+		FROM agent_profiles
+		ORDER BY name
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list profiles: %w", err)
+	}
+	defer rows.Close()
+
+	var profiles []*types.StoredProfile
+	for rows.Next() {
+		var sp types.StoredProfile
+		var jsonData string
+		if err := rows.Scan(&sp.Npub, &sp.Name, &sp.Description, &sp.Availability, &jsonData, &sp.UpdatedAt, &sp.DiscoveredAt); err != nil {
+			continue
+		}
+		profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+		if err != nil {
+			continue
+		}
+		sp.Profile = profile
+		profiles = append(profiles, &sp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return profiles, nil
+}
+
+// SearchProfiles searches profiles by name or capability
+func (p *DB) SearchProfiles(query string) ([]*types.StoredProfile, error) {
+	pattern := "%" + query + "%"
+	rows, err := p.db.Query(`
+		SELECT npub, name, description, availability, profile_json, updated_at, discovered_at
+		FROM agent_profiles
+		WHERE name LIKE ? OR description LIKE ? OR profile_json LIKE ?
+		ORDER BY name
+	`, pattern, pattern, pattern)
+	if err != nil {
+		return nil, fmt.Errorf("failed to search profiles: %w", err)
+	}
+	defer rows.Close()
+
+	var profiles []*types.StoredProfile
+	for rows.Next() {
+		var sp types.StoredProfile
+		var jsonData string
+		if err := rows.Scan(&sp.Npub, &sp.Name, &sp.Description, &sp.Availability, &jsonData, &sp.UpdatedAt, &sp.DiscoveredAt); err != nil {
+			continue
+		}
+		profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+		if err != nil {
+			continue
+		}
+		sp.Profile = profile
+		profiles = append(profiles, &sp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return profiles, nil
+}
+
+// DeleteProfile deletes a profile by npub
+func (p *DB) DeleteProfile(npub string) error {
+	_, err := p.db.Exec("DELETE FROM agent_profiles WHERE npub = ?", npub)
+	return err
+}
+
+// Close closes the database connection
+func (p *DB) Close() error {
+	return p.db.Close()
+}

internal/profile/profile.go
@@ -1,0 +1,108 @@
+package profile
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"fiatjaf.com/nostr"
+	"github.com/jason/agent-speaker/pkg/types"
+)
+
+const (
+	// ProfileKind is the nostr kind for agent profiles (Kind 30078 extension)
+	ProfileKind = 30078
+	// ProfileTag marks this as a profile event
+	ProfileTag = "profile"
+	// ProfileDTag is the 'd' tag value for parameterized replaceable events
+	ProfileDTag = "agent-profile"
+)
+
+// ProfileToEvent converts an AgentProfile to a nostr Event
+func ProfileToEvent(profile *types.AgentProfile, pubkey nostr.PubKey) (*nostr.Event, error) {
+	if err := profile.Validate(); err != nil {
+		return nil, err
+	}
+
+	content, err := json.Marshal(profile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal profile: %w", err)
+	}
+
+	event := &nostr.Event{
+		CreatedAt: nostr.Now(),
+		Kind:      ProfileKind,
+		Tags: nostr.Tags{
+			{"d", ProfileDTag},
+			{"c", ProfileTag},
+		},
+		Content: string(content),
+		PubKey:  pubkey,
+	}
+
+	return event, nil
+}
+
+// EventToProfile converts a nostr Event to an AgentProfile
+func EventToProfile(event *nostr.Event) (*types.AgentProfile, error) {
+	if event.Kind != ProfileKind {
+		return nil, fmt.Errorf("expected kind %d, got %d", ProfileKind, event.Kind)
+	}
+
+	// Verify it's a profile event
+	isProfile := false
+	for _, tag := range event.Tags {
+		if len(tag) >= 2 && tag[0] == "c" && tag[1] == ProfileTag {
+			isProfile = true
+			break
+		}
+	}
+	if !isProfile {
+		return nil, fmt.Errorf("event missing profile tag")
+	}
+
+	profile, err := types.AgentProfileFromJSON([]byte(event.Content))
+	if err != nil {
+		return nil, err
+	}
+
+	profile.UpdatedAt = int64(event.CreatedAt)
+	return profile, nil
+}
+
+// IsProfileEvent checks if a nostr event is an agent profile event
+func IsProfileEvent(event *nostr.Event) bool {
+	if event.Kind != ProfileKind {
+		return false
+	}
+	for _, tag := range event.Tags {
+		if len(tag) >= 2 && tag[0] == "c" && tag[1] == ProfileTag {
+			return true
+		}
+	}
+	return false
+}
+
+// BuildFilter creates a nostr filter for agent profile events
+func BuildFilter(authors []nostr.PubKey, limit int) nostr.Filter {
+	filter := nostr.Filter{
+		Kinds: []nostr.Kind{ProfileKind},
+		Tags:  nostr.TagMap{"c": []string{ProfileTag}},
+	}
+	if len(authors) > 0 {
+		filter.Authors = authors
+	}
+	if limit > 0 {
+		filter.Limit = limit
+	}
+	return filter
+}
+
+// NewAgentProfile creates a new agent profile with defaults
+func NewAgentProfile(name string) *types.AgentProfile {
+	return &types.AgentProfile{
+		Name:         name,
+		Version:      "1.0",
+		Availability: types.AvailabilityAvailable,
+		Capabilities: make([]types.Capability, 0),
+	}
+}

iDoris-ai/Hyphae

cmd/agent-speaker/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/AuraAIHQ/agent-speaker/internal/identity"
 	"github.com/AuraAIHQ/agent-speaker/internal/messaging"
 	"github.com/AuraAIHQ/agent-speaker/internal/nostr"
+	"github.com/AuraAIHQ/agent-speaker/internal/profile"
 	"github.com/AuraAIHQ/agent-speaker/internal/storage"
 	"github.com/urfave/cli/v3"
 )
@@ -44,6 +45,8 @@ func main() {
 			storage.StorageCmd,
 			// Group Chat
 			group.GroupCmd,
+			// Agent Profile
+			profile.ProfileCmd,
 			// Daemon
 			daemon.DaemonCmd,
 		},

internal/profile/db.go
@@ -1,0 +1,208 @@
+package profile
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/AuraAIHQ/agent-speaker/internal/storage"
+	"github.com/AuraAIHQ/agent-speaker/pkg/types"
+)
+
+// DB wraps profile database operations
+type DB struct {
+	db *sql.DB
+}
+
+// NewDB creates a new profile DB instance
+func NewDB() (*DB, error) {
+	db, err := storage.InitDB()
+	if err != nil {
+		return nil, err
+	}
+
+	p := &DB{db: db}
+	if err := p.migrate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// NewDBWithPath creates a new profile DB instance for testing
+func NewDBWithPath(dbPath string) (*DB, error) {
+	db, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	if _, err := db.Exec("PRAGMA foreign_keys = ON"); err != nil {
+		return nil, fmt.Errorf("failed to enable foreign keys: %w", err)
+	}
+
+	p := &DB{db: db}
+	if err := p.migrate(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// migrate creates profile tables
+func (p *DB) migrate() error {
+	_, err := p.db.Exec(`
+		CREATE TABLE IF NOT EXISTS agent_profiles (
+			npub TEXT PRIMARY KEY,
+			name TEXT NOT NULL,
+			description TEXT,
+			availability TEXT,
+			profile_json TEXT NOT NULL,
+			updated_at INTEGER NOT NULL,
+			discovered_at INTEGER NOT NULL
+		)
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to create agent_profiles table: %w", err)
+	}
+
+	indexes := []string{
+		`CREATE INDEX IF NOT EXISTS idx_profiles_name ON agent_profiles(name)`,
+		`CREATE INDEX IF NOT EXISTS idx_profiles_availability ON agent_profiles(availability)`,
+		`CREATE INDEX IF NOT EXISTS idx_profiles_updated ON agent_profiles(updated_at DESC)`,
+	}
+
+	for _, idx := range indexes {
+		if _, err := p.db.Exec(idx); err != nil {
+			return fmt.Errorf("failed to create index: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// StoreProfile stores or updates an agent profile
+func (p *DB) StoreProfile(npub string, profile *types.AgentProfile) error {
+	jsonData, err := json.Marshal(profile)
+	if err != nil {
+		return fmt.Errorf("failed to marshal profile: %w", err)
+	}
+
+	_, err = p.db.Exec(`
+		INSERT OR REPLACE INTO agent_profiles (npub, name, description, availability, profile_json, updated_at, discovered_at)
+		VALUES (?, ?, ?, ?, ?, ?, COALESCE((SELECT discovered_at FROM agent_profiles WHERE npub = ?), ?))
+	`, npub, profile.Name, profile.Description, profile.Availability, string(jsonData), profile.UpdatedAt, npub, profile.UpdatedAt)
+
+	if err != nil {
+		return fmt.Errorf("failed to store profile: %w", err)
+	}
+
+	return nil
+}
+
+// GetProfile retrieves a profile by npub
+func (p *DB) GetProfile(npub string) (*types.AgentProfile, error) {
+	var jsonData string
+	err := p.db.QueryRow(
+		"SELECT profile_json FROM agent_profiles WHERE npub = ?",
+		npub,
+	).Scan(&jsonData)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get profile: %w", err)
+	}
+
+	profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	return profile, nil
+}
+
+// ListProfiles lists all stored profiles
+func (p *DB) ListProfiles() ([]*types.StoredProfile, error) {
+	rows, err := p.db.Query(`
+		SELECT npub, name, description, availability, profile_json, updated_at, discovered_at
+		FROM agent_profiles
+		ORDER BY name
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list profiles: %w", err)
+	}
+	defer rows.Close()
+
+	var profiles []*types.StoredProfile
+	for rows.Next() {
+		var sp types.StoredProfile
+		var jsonData string
+		if err := rows.Scan(&sp.Npub, &sp.Name, &sp.Description, &sp.Availability, &jsonData, &sp.UpdatedAt, &sp.DiscoveredAt); err != nil {
+			continue
+		}
+		profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+		if err != nil {
+			continue
+		}
+		sp.Profile = profile
+		profiles = append(profiles, &sp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return profiles, nil
+}
+
+// SearchProfiles searches profiles by name or capability
+func (p *DB) SearchProfiles(query string) ([]*types.StoredProfile, error) {
+	pattern := "%" + query + "%"
+	rows, err := p.db.Query(`
+		SELECT npub, name, description, availability, profile_json, updated_at, discovered_at
+		FROM agent_profiles
+		WHERE name LIKE ? OR description LIKE ? OR profile_json LIKE ?
+		ORDER BY name
+	`, pattern, pattern, pattern)
+	if err != nil {
+		return nil, fmt.Errorf("failed to search profiles: %w", err)
+	}
+	defer rows.Close()
+
+	var profiles []*types.StoredProfile
+	for rows.Next() {
+		var sp types.StoredProfile
+		var jsonData string
+		if err := rows.Scan(&sp.Npub, &sp.Name, &sp.Description, &sp.Availability, &jsonData, &sp.UpdatedAt, &sp.DiscoveredAt); err != nil {
+			continue
+		}
+		profile, err := types.AgentProfileFromJSON([]byte(jsonData))
+		if err != nil {
+			continue
+		}
+		sp.Profile = profile
+		profiles = append(profiles, &sp)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return profiles, nil
+}
+
+// DeleteProfile deletes a profile by npub
+func (p *DB) DeleteProfile(npub string) error {
+	_, err := p.db.Exec("DELETE FROM agent_profiles WHERE npub = ?", npub)
+	return err
+}
+
+// Close closes the database connection
+func (p *DB) Close() error {
+	return p.db.Close()
+}

internal/profile/profile.go
@@ -1,0 +1,108 @@
+package profile
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"fiatjaf.com/nostr"
+	"github.com/AuraAIHQ/agent-speaker/pkg/types"
+)
+
+const (
+	// ProfileKind is the nostr kind for agent profiles (Kind 30078 extension)
+	ProfileKind = 30078
+	// ProfileTag marks this as a profile event
+	ProfileTag = "profile"
+	// ProfileDTag is the 'd' tag value for parameterized replaceable events
+	ProfileDTag = "agent-profile"
+)
+
+// ProfileToEvent converts an AgentProfile to a nostr Event
+func ProfileToEvent(profile *types.AgentProfile, pubkey nostr.PubKey) (*nostr.Event, error) {
+	if err := profile.Validate(); err != nil {
+		return nil, err
+	}
+
+	content, err := json.Marshal(profile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal profile: %w", err)
+	}
+
+	event := &nostr.Event{
+		CreatedAt: nostr.Now(),
+		Kind:      ProfileKind,
+		Tags: nostr.Tags{
+			{"d", ProfileDTag},
+			{"c", ProfileTag},
+		},
+		Content: string(content),
+		PubKey:  pubkey,
+	}
+
+	return event, nil
+}
+
+// EventToProfile converts a nostr Event to an AgentProfile
+func EventToProfile(event *nostr.Event) (*types.AgentProfile, error) {
+	if event.Kind != ProfileKind {
+		return nil, fmt.Errorf("expected kind %d, got %d", ProfileKind, event.Kind)
+	}
+
+	// Verify it's a profile event
+	isProfile := false
+	for _, tag := range event.Tags {
+		if len(tag) >= 2 && tag[0] == "c" && tag[1] == ProfileTag {
+			isProfile = true
+			break
+		}
+	}
+	if !isProfile {
+		return nil, fmt.Errorf("event missing profile tag")
+	}
+
+	profile, err := types.AgentProfileFromJSON([]byte(event.Content))
+	if err != nil {
+		return nil, err
+	}
+
+	profile.UpdatedAt = int64(event.CreatedAt)
+	return profile, nil
+}
+
+// IsProfileEvent checks if a nostr event is an agent profile event
+func IsProfileEvent(event *nostr.Event) bool {
+	if event.Kind != ProfileKind {
+		return false
+	}
+	for _, tag := range event.Tags {
+		if len(tag) >= 2 && tag[0] == "c" && tag[1] == ProfileTag {
+			return true
+		}
+	}
+	return false
+}
+
+// BuildFilter creates a nostr filter for agent profile events
+func BuildFilter(authors []nostr.PubKey, limit int) nostr.Filter {
+	filter := nostr.Filter{
+		Kinds: []nostr.Kind{ProfileKind},
+		Tags:  nostr.TagMap{"c": []string{ProfileTag}},
+	}
+	if len(authors) > 0 {
+		filter.Authors = authors
+	}
+	if limit > 0 {
+		filter.Limit = limit
+	}
+	return filter
+}
+
+// NewAgentProfile creates a new agent profile with defaults
+func NewAgentProfile(name string) *types.AgentProfile {
+	return &types.AgentProfile{
+		Name:         name,
+		Version:      "1.0",
+		Availability: types.AvailabilityAvailable,
+		Capabilities: make([]types.Capability, 0),
+	}
+}