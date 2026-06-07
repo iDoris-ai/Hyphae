AuraAIHQ/agent-speaker

feat(tui): add TUI chat interface with Bubble Tea
Features:
- New internal/tui package with Bubble Tea framework
- tui chat --with <contact>: Interactive chat interface
- tui contacts: Contact list with navigation
- Real-time message display with timestamps
- Encryption indicators (🔒)
- Keyboard shortcuts: ↑/↓, pgup/pgdn, enter, esc

Dependencies:
- github.com/charmbracelet/bubbletea
- github.com/charmbracelet/bubbles (textinput, viewport)
- github.com/charmbracelet/lipgloss (styling)

Tests:
- 8 unit tests for TUI models
- 3 E2E tests for TUI commands
- All tests passing

Usage:
  agent-speaker tui contacts     # Browse contacts
  agent-speaker tui chat --with bob  # Chat with contact

cmd/agent-speaker/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/jason/agent-speaker/internal/messaging"
 	"github.com/jason/agent-speaker/internal/nostr"
 	"github.com/jason/agent-speaker/internal/storage"
+	"github.com/jason/agent-speaker/internal/tui"
 	"github.com/urfave/cli/v3"
 )
 
@@ -41,6 +42,8 @@ func main() {
 			messaging.HistoryCmd,
 			// Storage
 			storage.StorageCmd,
+			// TUI
+			tui.TUICmd,
 			// Daemon
 			daemon.DaemonCmd,
 		},

internal/tui/contacts.go
@@ -1,0 +1,153 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/jason/agent-speaker/internal/identity"
+	"github.com/jason/agent-speaker/pkg/types"
+)
+
+// ContactsModel represents the contacts list TUI
+type ContactsModel struct {
+	contacts   []*types.Contact
+	identities []*types.Identity
+	cursor     int
+	selected   string
+	width      int
+	height     int
+	err        error
+}
+
+// NewContactsModel creates a new contacts model
+func NewContactsModel() (*ContactsModel, error) {
+	ks, err := identity.LoadKeyStore()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ContactsModel{
+		contacts:   identity.ListContacts(ks),
+		identities: identity.ListIdentities(ks),
+		cursor:     0,
+	}, nil
+}
+
+// Init initializes the model
+func (m *ContactsModel) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles messages
+func (m *ContactsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			total := len(m.contacts) + len(m.identities)
+			if m.cursor < total-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			// Selection would launch chat
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	}
+
+	return m, nil
+}
+
+// View renders the UI
+func (m *ContactsModel) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("Error: %v\n\nPress any key to exit...", m.err)
+	}
+
+	var b strings.Builder
+
+	// Title
+	title := titleStyle.Render("📇 Contacts & Identities")
+	b.WriteString(title)
+	b.WriteString("\n\n")
+
+	// Identities section
+	b.WriteString(lipgloss.NewStyle().Bold(true).Render("👤 Your Identities:"))
+	b.WriteString("\n")
+
+	for i, id := range m.identities {
+		cursor := " "
+		if m.cursor == i {
+			cursor = "▶"
+		}
+
+		line := fmt.Sprintf("%s %s (%s...)",
+			cursor,
+			id.Nickname,
+			id.Npub[:16],
+		)
+
+		if m.cursor == i {
+			line = lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4")).Render(line)
+		}
+
+		b.WriteString(line)
+		b.WriteString("\n")
+	}
+
+	b.WriteString("\n")
+
+	// Contacts section
+	b.WriteString(lipgloss.NewStyle().Bold(true).Render("📇 Contacts:"))
+	b.WriteString("\n")
+
+	offset := len(m.identities)
+	for i, contact := range m.contacts {
+		idx := offset + i
+		cursor := " "
+		if m.cursor == idx {
+			cursor = "▶"
+		}
+
+		line := fmt.Sprintf("%s %s (%s...)",
+			cursor,
+			contact.Nickname,
+			contact.Npub[:16],
+		)
+
+		if m.cursor == idx {
+			line = lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4")).Render(line)
+		}
+
+		b.WriteString(line)
+		b.WriteString("\n")
+	}
+
+	if len(m.contacts) == 0 {
+		b.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#666666")).Render("  No contacts. Add one with:"))
+		b.WriteString("\n")
+		b.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#666666")).Render("  agent-speaker contact add --nickname <name> --npub <npub>"))
+		b.WriteString("\n")
+	}
+
+	// Help
+	b.WriteString("\n")
+	help := helpStyle.Render("↑/↓: navigate • enter: select • q/esc: quit")
+	b.WriteString(help)
+
+	return b.String()
+}

internal/tui/model_test.go
@@ -1,0 +1,257 @@
+package tui
+
+import (
+	"os"
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jason/agent-speaker/internal/identity"
+	"github.com/jason/agent-speaker/internal/storage"
+	"github.com/jason/agent-speaker/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func setupTestEnv(t *testing.T) func() {
+	tempDir := t.TempDir()
+	os.Setenv("HOME", tempDir)
+
+	// Initialize storage
+	_, err := storage.InitDB()
+	require.NoError(t, err)
+
+	// Create test identity
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "testuser")
+	if err != nil {
+		// Identity might already exist
+	}
+
+	return func() {
+		storage.CloseDB()
+		os.RemoveAll(tempDir)
+		os.Unsetenv("HOME")
+	}
+}
+
+func TestNewChatModel(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	// First create identity to use as contact
+	_, err = identity.CreateIdentity(ks, "testcontact")
+	require.NoError(t, err)
+
+	// Reload to get the identity
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "testcontact")
+	if id != nil {
+		// Use as contact
+		err = identity.AddContact(ks, "testcontact", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("testcontact")
+	require.NoError(t, err)
+	assert.NotNil(t, model)
+	assert.Equal(t, "testcontact", model.contactName)
+	assert.NotNil(t, model.store)
+	assert.NotNil(t, model.viewport)
+	assert.NotNil(t, model.input)
+}
+
+func TestNewChatModelContactNotFound(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	_, err := NewChatModel("nonexistent")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "not found")
+}
+
+func TestChatModelInit(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactinit")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactinit")
+	if id != nil {
+		err = identity.AddContact(ks, "contactinit", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactinit")
+	require.NoError(t, err)
+
+	cmd := model.Init()
+	assert.NotNil(t, cmd)
+}
+
+func TestChatModelUpdateWindowSize(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactws")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactws")
+	if id != nil {
+		err = identity.AddContact(ks, "contactws", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactws")
+	require.NoError(t, err)
+
+	// Simulate window resize
+	newModel, _ := model.Update(tea.WindowSizeMsg{Width: 100, Height: 30})
+	m := newModel.(*ChatModel)
+
+	assert.Equal(t, 100, m.width)
+	assert.Equal(t, 30, m.height)
+}
+
+func TestChatModelQuit(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactquit")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactquit")
+	if id != nil {
+		err = identity.AddContact(ks, "contactquit", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactquit")
+	require.NoError(t, err)
+
+	// Press escape to quit
+	_, cmd := model.Update(tea.KeyMsg{Type: tea.KeyEsc})
+	assert.NotNil(t, cmd) // Should return a quit command
+}
+
+func TestNewContactsModel(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test identity and contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "myidentity")
+	require.NoError(t, err)
+
+	model, err := NewContactsModel()
+	require.NoError(t, err)
+	assert.NotNil(t, model)
+	assert.GreaterOrEqual(t, len(model.identities), 1)
+}
+
+func TestContactsModelNavigation(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create multiple identities
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "id1")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "id2")
+	require.NoError(t, err)
+
+	model, err := NewContactsModel()
+	require.NoError(t, err)
+
+	// Test navigation down
+	newModel, _ := model.Update(tea.KeyMsg{Type: tea.KeyDown})
+	m := newModel.(*ContactsModel)
+	assert.Equal(t, 1, m.cursor)
+
+	// Test navigation up
+	newModel, _ = m.Update(tea.KeyMsg{Type: tea.KeyUp})
+	m = newModel.(*ContactsModel)
+	assert.Equal(t, 0, m.cursor)
+
+	// Test quit
+	_, cmd := m.Update(tea.KeyMsg{Type: tea.KeyEsc})
+	assert.NotNil(t, cmd) // Should return a quit command
+}
+
+func TestFormatMessage(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactfmt")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactfmt")
+	if id != nil {
+		err = identity.AddContact(ks, "contactfmt", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactfmt")
+	require.NoError(t, err)
+
+	// Test incoming message
+	msg := model.formatMessage(types.StoredMessage{
+		Plaintext:   "Hello",
+		IsIncoming:  true,
+		IsEncrypted: false,
+		CreatedAt:   1234567890,
+	})
+	assert.Contains(t, msg, "Hello")
+	assert.NotContains(t, msg, "🔒")
+
+	// Test outgoing encrypted message
+	msg = model.formatMessage(types.StoredMessage{
+		Plaintext:   "Secret",
+		IsIncoming:  false,
+		IsEncrypted: true,
+		CreatedAt:   1234567890,
+	})
+	assert.Contains(t, msg, "Secret")
+	assert.Contains(t, msg, "🔒")
+}

iDoris-ai/Hyphae

cmd/agent-speaker/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/AuraAIHQ/agent-speaker/internal/nostr"
 	"github.com/AuraAIHQ/agent-speaker/internal/profile"
 	"github.com/AuraAIHQ/agent-speaker/internal/storage"
+	"github.com/AuraAIHQ/agent-speaker/internal/tui"
 	"github.com/urfave/cli/v3"
 )
 
@@ -47,6 +48,8 @@ func main() {
 			group.GroupCmd,
 			// Agent Profile
 			profile.ProfileCmd,
+			// TUI
+			tui.TUICmd,
 			// Daemon
 			daemon.DaemonCmd,
 		},

internal/tui/contacts.go
@@ -1,0 +1,153 @@
+package tui
+
+import (
+	"fmt"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/AuraAIHQ/agent-speaker/internal/identity"
+	"github.com/AuraAIHQ/agent-speaker/pkg/types"
+)
+
+// ContactsModel represents the contacts list TUI
+type ContactsModel struct {
+	contacts   []*types.Contact
+	identities []*types.Identity
+	cursor     int
+	selected   string
+	width      int
+	height     int
+	err        error
+}
+
+// NewContactsModel creates a new contacts model
+func NewContactsModel() (*ContactsModel, error) {
+	ks, err := identity.LoadKeyStore()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ContactsModel{
+		contacts:   identity.ListContacts(ks),
+		identities: identity.ListIdentities(ks),
+		cursor:     0,
+	}, nil
+}
+
+// Init initializes the model
+func (m *ContactsModel) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles messages
+func (m *ContactsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			total := len(m.contacts) + len(m.identities)
+			if m.cursor < total-1 {
+				m.cursor++
+			}
+
+		case "enter":
+			// Selection would launch chat
+			return m, tea.Quit
+		}
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	}
+
+	return m, nil
+}
+
+// View renders the UI
+func (m *ContactsModel) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("Error: %v\n\nPress any key to exit...", m.err)
+	}
+
+	var b strings.Builder
+
+	// Title
+	title := titleStyle.Render("📇 Contacts & Identities")
+	b.WriteString(title)
+	b.WriteString("\n\n")
+
+	// Identities section
+	b.WriteString(lipgloss.NewStyle().Bold(true).Render("👤 Your Identities:"))
+	b.WriteString("\n")
+
+	for i, id := range m.identities {
+		cursor := " "
+		if m.cursor == i {
+			cursor = "▶"
+		}
+
+		line := fmt.Sprintf("%s %s (%s...)",
+			cursor,
+			id.Nickname,
+			id.Npub[:16],
+		)
+
+		if m.cursor == i {
+			line = lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4")).Render(line)
+		}
+
+		b.WriteString(line)
+		b.WriteString("\n")
+	}
+
+	b.WriteString("\n")
+
+	// Contacts section
+	b.WriteString(lipgloss.NewStyle().Bold(true).Render("📇 Contacts:"))
+	b.WriteString("\n")
+
+	offset := len(m.identities)
+	for i, contact := range m.contacts {
+		idx := offset + i
+		cursor := " "
+		if m.cursor == idx {
+			cursor = "▶"
+		}
+
+		line := fmt.Sprintf("%s %s (%s...)",
+			cursor,
+			contact.Nickname,
+			contact.Npub[:16],
+		)
+
+		if m.cursor == idx {
+			line = lipgloss.NewStyle().Foreground(lipgloss.Color("#7D56F4")).Render(line)
+		}
+
+		b.WriteString(line)
+		b.WriteString("\n")
+	}
+
+	if len(m.contacts) == 0 {
+		b.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#666666")).Render("  No contacts. Add one with:"))
+		b.WriteString("\n")
+		b.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#666666")).Render("  agent-speaker contact add --nickname <name> --npub <npub>"))
+		b.WriteString("\n")
+	}
+
+	// Help
+	b.WriteString("\n")
+	help := helpStyle.Render("↑/↓: navigate • enter: select • q/esc: quit")
+	b.WriteString(help)
+
+	return b.String()
+}

internal/tui/model_test.go
@@ -1,0 +1,257 @@
+package tui
+
+import (
+	"os"
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/AuraAIHQ/agent-speaker/internal/identity"
+	"github.com/AuraAIHQ/agent-speaker/internal/storage"
+	"github.com/AuraAIHQ/agent-speaker/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func setupTestEnv(t *testing.T) func() {
+	tempDir := t.TempDir()
+	os.Setenv("HOME", tempDir)
+
+	// Initialize storage
+	_, err := storage.InitDB()
+	require.NoError(t, err)
+
+	// Create test identity
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "testuser")
+	if err != nil {
+		// Identity might already exist
+	}
+
+	return func() {
+		storage.CloseDB()
+		os.RemoveAll(tempDir)
+		os.Unsetenv("HOME")
+	}
+}
+
+func TestNewChatModel(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	// First create identity to use as contact
+	_, err = identity.CreateIdentity(ks, "testcontact")
+	require.NoError(t, err)
+
+	// Reload to get the identity
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "testcontact")
+	if id != nil {
+		// Use as contact
+		err = identity.AddContact(ks, "testcontact", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("testcontact")
+	require.NoError(t, err)
+	assert.NotNil(t, model)
+	assert.Equal(t, "testcontact", model.contactName)
+	assert.NotNil(t, model.store)
+	assert.NotNil(t, model.viewport)
+	assert.NotNil(t, model.input)
+}
+
+func TestNewChatModelContactNotFound(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	_, err := NewChatModel("nonexistent")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "not found")
+}
+
+func TestChatModelInit(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactinit")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactinit")
+	if id != nil {
+		err = identity.AddContact(ks, "contactinit", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactinit")
+	require.NoError(t, err)
+
+	cmd := model.Init()
+	assert.NotNil(t, cmd)
+}
+
+func TestChatModelUpdateWindowSize(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactws")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactws")
+	if id != nil {
+		err = identity.AddContact(ks, "contactws", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactws")
+	require.NoError(t, err)
+
+	// Simulate window resize
+	newModel, _ := model.Update(tea.WindowSizeMsg{Width: 100, Height: 30})
+	m := newModel.(*ChatModel)
+
+	assert.Equal(t, 100, m.width)
+	assert.Equal(t, 30, m.height)
+}
+
+func TestChatModelQuit(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactquit")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactquit")
+	if id != nil {
+		err = identity.AddContact(ks, "contactquit", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactquit")
+	require.NoError(t, err)
+
+	// Press escape to quit
+	_, cmd := model.Update(tea.KeyMsg{Type: tea.KeyEsc})
+	assert.NotNil(t, cmd) // Should return a quit command
+}
+
+func TestNewContactsModel(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create test identity and contact
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "myidentity")
+	require.NoError(t, err)
+
+	model, err := NewContactsModel()
+	require.NoError(t, err)
+	assert.NotNil(t, model)
+	assert.GreaterOrEqual(t, len(model.identities), 1)
+}
+
+func TestContactsModelNavigation(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	// Create multiple identities
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "id1")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "id2")
+	require.NoError(t, err)
+
+	model, err := NewContactsModel()
+	require.NoError(t, err)
+
+	// Test navigation down
+	newModel, _ := model.Update(tea.KeyMsg{Type: tea.KeyDown})
+	m := newModel.(*ContactsModel)
+	assert.Equal(t, 1, m.cursor)
+
+	// Test navigation up
+	newModel, _ = m.Update(tea.KeyMsg{Type: tea.KeyUp})
+	m = newModel.(*ContactsModel)
+	assert.Equal(t, 0, m.cursor)
+
+	// Test quit
+	_, cmd := m.Update(tea.KeyMsg{Type: tea.KeyEsc})
+	assert.NotNil(t, cmd) // Should return a quit command
+}
+
+func TestFormatMessage(t *testing.T) {
+	cleanup := setupTestEnv(t)
+	defer cleanup()
+
+	ks, err := identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	_, err = identity.CreateIdentity(ks, "contactfmt")
+	require.NoError(t, err)
+
+	ks, err = identity.LoadKeyStore()
+	require.NoError(t, err)
+
+	id, _ := identity.GetIdentity(ks, "contactfmt")
+	if id != nil {
+		err = identity.AddContact(ks, "contactfmt", id.Npub)
+		require.NoError(t, err)
+	}
+
+	model, err := NewChatModel("contactfmt")
+	require.NoError(t, err)
+
+	// Test incoming message
+	msg := model.formatMessage(types.StoredMessage{
+		Plaintext:   "Hello",
+		IsIncoming:  true,
+		IsEncrypted: false,
+		CreatedAt:   1234567890,
+	})
+	assert.Contains(t, msg, "Hello")
+	assert.NotContains(t, msg, "🔒")
+
+	// Test outgoing encrypted message
+	msg = model.formatMessage(types.StoredMessage{
+		Plaintext:   "Secret",
+		IsIncoming:  false,
+		IsEncrypted: true,
+		CreatedAt:   1234567890,
+	})
+	assert.Contains(t, msg, "Secret")
+	assert.Contains(t, msg, "🔒")
+}